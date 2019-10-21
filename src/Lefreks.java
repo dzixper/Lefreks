1bc0bf8c7a9db9327819f9c87ebdfd59deb1806c
dzixper <olcen.szymon@gmail.com>
2019-10-21 22:09:06 +0200

    First menu project, testing BorderPane

--- src/Lefreks.java
+++ src/Lefreks.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -15,21 +17,51 @@ import javafx.stage.Stage;
 public class Lefreks extends Application {
     NumberHandler numberHandler = new NumberHandler();
 
+    Scene menuScene, gameScene, scoreboardScene;
+
     @Override
     public void start(Stage stage) {
         // Resolution variables
         int RESX = 1024,
-                RESY = 768,
-                INGAME_MENU_SIZE = 90;
+            RESY = 768,
+            INGAME_MENU_SIZE = 90;
         stage.setResizable(false);
 
-        // Playing area
+
+        //
+        // MENU SECTION
+        //
+
+        BorderPane menuArea = new BorderPane();
+
+        Button playButton = new Button("Play");
+        Button leaderboardButton = new Button("Leaderboard");
+        Button exitButton = new Button("Exit");
+
+        VBox vbox = new VBox();
+        vbox.setSpacing(15);
+
+        vbox.getChildren().addAll(playButton, leaderboardButton, exitButton);
+        vbox.setAlignment(Pos.CENTER);
+        menuArea.setCenter(vbox);
+
+        // Area styling
+        menuArea.setBackground(new Background(new BackgroundFill(Color.rgb(255,251,235), CornerRadii.EMPTY, Insets.EMPTY)));
+        menuScene = new Scene (menuArea, RESX, RESY);
+
+
+
+
+        //
+        // PLAYING AREA SECTION
+        //
+
+        // Playing area init
         Pane playingArea = new Pane();
 
 
         // Game setting variables
-        int targetSize = 40; // TODO -- DEBUG VALUE
-
+        int targetSize = 100;
 
         // Ingame menu background
         Rectangle guiBar = new Rectangle(RESX, INGAME_MENU_SIZE, Color.rgb(71, 59, 59));
@@ -50,7 +82,7 @@ public class Lefreks extends Application {
         textTimer.setFill(Color.INDIANRED);
         textTimer.textProperty().bind(numberHandler.getText());
 
-        // Target
+        // Target placed at the centre of the screen
         Circle target = new Circle(RESX / 2, RESY / 2, targetSize, Color.INDIANRED);
 
         // Mouse click event handler
@@ -78,11 +110,16 @@ public class Lefreks extends Application {
         // Add shapes to a group
         playingArea.getChildren().addAll(guiBar, menuButton, target, textPoints, textTimer);
 
-        // Game window styling
-        Scene gameScene = new Scene(playingArea, RESX, RESY);
-        gameScene.setFill(Color.rgb(255, 251, 235));
+        // Area styling
+        playingArea.setBackground(new Background(new BackgroundFill(Color.rgb(255,251,235), CornerRadii.EMPTY, Insets.EMPTY)));
+        gameScene = new Scene(playingArea, RESX, RESY);
+
+        //
+        // STAGE CONFIG
+        //
+
         stage.setTitle("Lefreks");
-        stage.setScene(gameScene);
+        stage.setScene(menuScene);
         stage.show();
     }
 
